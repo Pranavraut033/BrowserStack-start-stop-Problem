baf85da55b332311eaba8d6dce4ec5757a64ee6d
Pranav Raut <pranavraut033@gmail.com>
2020-08-01 11:26:21 +0530

    init
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+import logger from "morgan";
+import router from "./router";
+import express from "express";
+import queryPaser from "./utils/queryPaser";
+
+import { urlencoded, json } from "body-parser";
+
+global.APP_ROOT = process.cwd();
+
+const app = express();
+
+// app.set("trust proxy", 1); // trust first proxy
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(queryPaser);
+app.user(router);
+
+app.use("*", (_, res) => res.status(404).send(res, "Page not found"));
+
+// listen for requests
+app.listen(process.env.PORT, () =>
+  console.log("Server is listening on port " + process.env.PORT)
+);
+
+app.addListener("close", (err) =>
+  console.log("Port '" + process.env.port + "' not available: " + err)
+);
---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = new Router();
+
+router.use("/start/");
+
+export default router;
